7179ab18057d91677383e2f23143123b42c2ea1e
TranTyPhuQuy <20130377@st.hcmuaf.edu.vn>
2023-12-04 21:14:48 +0700

    create method loading()

--- src/data_warehouse/LoadDWToDMART.java
+++ src/data_warehouse/LoadDWToDMART.java
@@ -23,6 +23,7 @@ public class LoadDWToDMART {
         }
         return Date.valueOf("1111-11-11");
     }
+
     private void insertDbMart(Connection conn, ResultSet resultSet) throws SQLException {
         System.out.println("insert..." + resultSet.getInt(1) + "-" + resultSet.getInt(5));
         String sql = "INSERT INTO gold (fact_gold_id, type, buy, sell, area_id, area_name, date, time) " +
@@ -34,11 +35,34 @@ public class LoadDWToDMART {
             statement.setString(4, resultSet.getString(4));
             statement.setInt(5, resultSet.getInt(5));
             statement.setString(6, resultSet.getString(6));
-            statement.setDate(7, Date.valueOf(resultSet.getString(9)+"-"+resultSet.getString(8)+"-"+resultSet.getString(7)));
-            statement.setTime(8, Time.valueOf(resultSet.getString(10)+":"+resultSet.getString(11)+":00"));
+            statement.setDate(7, Date.valueOf(resultSet.getString(9) + "-" + resultSet.getString(8) + "-" + resultSet.getString(7)));
+            statement.setTime(8, Time.valueOf(resultSet.getString(10) + ":" + resultSet.getString(11) + ":00"));
             statement.executeUpdate();
         }
     }
+
+    private void loading(Connection connWH, Connection connMART, Connection connCONTROL, int areaId, Date date) throws SQLException {
+        System.out.println("Loading..." + areaId + "-" + date);
+
+        String sql = "SELECT f.id AS fact_gold_id, t.type, f.buy, f.sell, f.id_area AS area_id, " +
+                "a.name AS area_name, d.day, d.month, d.year, d.hour, d.minute " +
+                "FROM fact_gold_price f " +
+                "INNER JOIN dim_date d ON f.id_date = d.id " +
+                "INNER JOIN dim_gold_type t ON t.id = f.id_gold_type " +
+                "INNER JOIN dim_area a ON a.id = f.id_area " +
+                "WHERE d.id = ?";
+        try (PreparedStatement statement = connWH.prepareStatement(sql)) {
+            statement.setDate(1, date);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    insertDbMart(connMART, resultSet);
+                }
+            }
+            insertLog(connCONTROL, date);
+        }
+
+    }
+
     private void insertLog(Connection conn, Date date) throws SQLException {
         System.out.println("insertLog..." + date);
         String sql = "INSERT INTO log (id_config, time_extract, id_date, status_extract) VALUES (?, ?, ?, ?)";
