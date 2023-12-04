021f73b11a91dd4b0aa83041ff99c1a439717297
TranTyPhuQuy <20130377@st.hcmuaf.edu.vn>
2023-12-04 21:11:43 +0700

    create method insertDbMart()

--- src/data_warehouse/LoadDWToDMART.java
+++ src/data_warehouse/LoadDWToDMART.java
@@ -23,6 +23,22 @@ public class LoadDWToDMART {
         }
         return Date.valueOf("1111-11-11");
     }
+    private void insertDbMart(Connection conn, ResultSet resultSet) throws SQLException {
+        System.out.println("insert..." + resultSet.getInt(1) + "-" + resultSet.getInt(5));
+        String sql = "INSERT INTO gold (fact_gold_id, type, buy, sell, area_id, area_name, date, time) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setInt(1, resultSet.getInt(1));
+            statement.setString(2, resultSet.getString(2));
+            statement.setString(3, resultSet.getString(3));
+            statement.setString(4, resultSet.getString(4));
+            statement.setInt(5, resultSet.getInt(5));
+            statement.setString(6, resultSet.getString(6));
+            statement.setDate(7, Date.valueOf(resultSet.getString(9)+"-"+resultSet.getString(8)+"-"+resultSet.getString(7)));
+            statement.setTime(8, Time.valueOf(resultSet.getString(10)+":"+resultSet.getString(11)+":00"));
+            statement.executeUpdate();
+        }
+    }
     private void insertLog(Connection conn, Date date) throws SQLException {
         System.out.println("insertLog..." + date);
         String sql = "INSERT INTO log (id_config, time_extract, id_date, status_extract) VALUES (?, ?, ?, ?)";
